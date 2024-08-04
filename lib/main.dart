df402befbe6e9773910ded77aa2966fca34edb4a
tejas <tejasgathekar78@gmail.com>
2024-08-04 14:35:04 +0530

    go router added

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,13 @@ import 'package:porfoliov7/bloc/data_collector/data_collector_event.dart';
 import 'package:porfoliov7/screens/components/theme/theme_data.dart';
 import 'package:porfoliov7/screens/home_screen/home_screen.dart';
 import 'package:porfoliov7/screens/privacy_policy/privacy_policy.dart';
+import 'package:url_strategy/url_strategy.dart';
+import 'package:go_router/go_router.dart';
 
-void main() => runApp(const Starter());
+void main() {
+  setPathUrlStrategy();
+  runApp(const Starter());
+}
 
 class Starter extends StatelessWidget {
   const Starter({super.key});
@@ -17,15 +22,27 @@ class Starter extends StatelessWidget {
       create: (_) => DataCollectorBloc()..add(DataCollectionInitEvent()),
       child: MaterialApp.router(
         debugShowCheckedModeBanner: false,
-        routerDelegate: MyRouterDelegate(),
-        routeInformationParser: MyRouteInformationParser(),
-        routeInformationProvider: PlatformRouteInformationProvider(
-            initialRouteInformation: RouteInformation(uri: Uri.parse("/"))),
+        routerConfig: _router,
         theme: CustomThemes.lightTheme,
       ),
     );
   }
 }
+
+final GoRouter _router = GoRouter(
+  initialLocation: '/home',
+  routes: [
+    GoRoute(
+      path: '/home',
+      builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: '/ppt',
+      builder: (context, state) => const PrivacyPolicy(),
+    ),
+  ],
+  errorBuilder: (context, state) => const ErrorScreen(),
+);
 
 class ErrorScreen extends StatelessWidget {
   const ErrorScreen({super.key});
@@ -39,83 +56,6 @@ class ErrorScreen extends StatelessWidget {
           style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
         ),
       ),
-    );
-  }
-}
-
-abstract class MyRoutePath {}
-
-class HomePath extends MyRoutePath {}
-
-class PrivacyPolicyPath extends MyRoutePath {}
-
-class InvalidPath extends MyRoutePath {}
-
-class MyRouteInformationParser extends RouteInformationParser<MyRoutePath> {
-  @override
-  Future<MyRoutePath> parseRouteInformation(
-      RouteInformation routeInformation) async {
-    final uri = routeInformation.uri;
-    if (uri.pathSegments.isEmpty) {
-      return HomePath();
-    } else if (uri.pathSegments[0] == 'ppt') {
-      return PrivacyPolicyPath();
-    }
-    return InvalidPath();
-  }
-
-  @override
-  RouteInformation? restoreRouteInformation(MyRoutePath configuration) {
-    if (configuration is HomePath) {
-      return RouteInformation(uri: Uri.parse('/'));
-    } else if (configuration is PrivacyPolicyPath) {
-      return RouteInformation(uri: Uri.parse('/ppt'));
-    }
-    return RouteInformation(uri: Uri.parse('/invalid'));
-  }
-}
-
-class MyRouterDelegate extends RouterDelegate<MyRoutePath>
-    with ChangeNotifier, PopNavigatorRouterDelegateMixin<MyRoutePath> {
-  MyRoutePath? _currentPath;
-  MyRouterDelegate() : navigatorKey = GlobalKey<NavigatorState>();
-
-  @override
-  MyRoutePath? get currentConfiguration => _currentPath;
-
-  @override
-  final GlobalKey<NavigatorState> navigatorKey;
-
-  @override
-  Future<void> setNewRoutePath(MyRoutePath configuration) async {
-    _currentPath = configuration;
-    notifyListeners();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Navigator(
-      key: navigatorKey,
-      pages: [
-        const MaterialPage(child: HomeScreen(), key: ValueKey("HomePage")),
-        if (_currentPath is PrivacyPolicyPath)
-          const MaterialPage(
-              child: PrivacyPolicy(), key: ValueKey("PrivacyPolicy")),
-        if (_currentPath is InvalidPath)
-          const MaterialPage(child: ErrorScreen(), key: ValueKey("Invalid"))
-      ],
-      onPopPage: (route, result) {
-        if (!route.didPop(result)) {
-          return false;
-        }
-        if (_currentPath is PrivacyPolicyPath) {
-          _currentPath = PrivacyPolicyPath();
-          notifyListeners();
-        } else {
-          _currentPath = InvalidPath();
-        }
-        return true;
-      },
     );
   }
 }
