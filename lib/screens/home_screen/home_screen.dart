4073b6f9bb5cb9c834e121488266dc972a7bef13
tejas <tejasgathekar78@gmail.com>
2024-08-04 19:33:33 +0530

    url changes

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:porfoliov7/bloc/data_collector/data_collector_bloc.dart';
 import 'package:porfoliov7/bloc/data_collector/data_collector_state.dart';
 import 'package:porfoliov7/networks/data_networks_nwt.dart';
 import 'package:porfoliov7/screens/components/custom_components.dart';
-import 'package:shimmer/shimmer.dart';
 import '../components/constant/color_constant.dart';
-import '../components/constant/size_constant.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -16,7 +13,8 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateMixin {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
   late AnimationController animationController;
   late Animation animationTop;
   final dataNetworks = DataNetworks();
@@ -24,7 +22,8 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
     animationTop = Tween(begin: 60.0, end: 80.0).animate(animationController);
     animationController.repeat(reverse: true);
   }
@@ -34,7 +33,6 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
     animationController.dispose();
     super.dispose();
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -70,22 +68,30 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
                               decoration: BoxDecoration(
                                   color: ColorConstant.navBarColorLeft,
                                   border: Border(
-                                    bottom: BorderSide(color: Colors.grey.shade300),
+                                    bottom:
+                                        BorderSide(color: Colors.grey.shade300),
                                   ),
                                   borderRadius: const BorderRadius.only(
                                       bottomRight: Radius.circular(100),
                                       bottomLeft: Radius.circular(100))),
-                              child: BlocBuilder<DataCollectorBloc, DataCollectorState>(
+                              child: BlocBuilder<DataCollectorBloc,
+                                  DataCollectorState>(
                                 builder: (context, state) {
                                   if (state is DataCollectionInitState) {
-                                    return const Center(child: Text("Loading.."));
-                                  } else if (state is DataCollectionCompletedState) {
+                                    return const Center(
+                                        child: Text("Loading.."));
+                                  } else if (state
+                                      is DataCollectionCompletedState) {
                                     return SingleChildScrollView(
                                       child: Column(
                                         children: [
                                           const SizedBox(height: 10),
-                                          CustomAppBar(width: size.maxWidth, state: state, dataNetworks: dataNetworks),
-                                          ProfilePic(animationTop: animationTop),
+                                          CustomAppBar(
+                                              width: size.maxWidth,
+                                              state: state,
+                                              dataNetworks: dataNetworks),
+                                          ProfilePic(
+                                              animationTop: animationTop),
                                           ShowIntro(width: size.maxWidth),
                                           const SizedBox(height: 70),
                                           ShowButton(dataNetworks: dataNetworks)
@@ -97,10 +103,28 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
                                   }
                                 },
                               )),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              const SizedBox(
+                                height: 200,
+                              ),
+                              Stack(
+                                children: [
+                                  Container(
+                                    child: Transform.rotate(
+                                        angle: -0.3, child:  Text("Projects",style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Colors.black))),
+                                  )
+                                ],
+                              )
+                            ],
+                          )
                         ],
                       ),
                     ),
-                    const SizedBox(height: 400),
+
+                    ///TODO:center black container
+                    const SizedBox(height: 600),
                     Container(
                       padding: const EdgeInsets.all(40),
                       alignment: Alignment.bottomCenter,
@@ -121,7 +145,8 @@ class _HomeScreenState extends State<HomeScreen> with SingleTickerProviderStateM
                               endIndent: size.maxWidth / 7,
                               color: Colors.grey.shade300),
                           const SizedBox(height: 20),
-                          Text("©2024 All right reserved.", style: Theme.of(context).textTheme.bodyMedium),
+                          Text("©2024 All right reserved.",
+                              style: Theme.of(context).textTheme.bodyMedium),
                         ],
                       ),
                     ),
